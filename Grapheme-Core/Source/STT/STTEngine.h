9e160908db6ea4bf4128ec0315d1eabc334c1ce0
cpap7 <mjajarmi2000@outlook.com>
2026-03-14 13:30:21 -0400

    Added core speech transcription engine class, progress tracker, config structs, & result structs

---
+++ Grapheme-Core/Source/STT/STTEngine.h
@@ -0,0 +1,51 @@
+#pragma once
+#include "STTConfig.h"
+#include "STTProgressTracker.h"
+#include "STTResults.h"
+
+#include <vector>
+#include <memory>
+
+// Forward declarations
+struct whisper_context;
+struct whisper_full_params;
+
+namespace Grapheme {
+	
+	class CSTTEngineImpl {
+	private:
+		SSTTConfig m_config;
+		CSTTProgressTracker m_progress_tracker;
+
+		// whisper.cpp 
+		whisper_context* m_context = nullptr;
+
+	public:
+		explicit CSTTEngineImpl(const SSTTConfig& a_config);
+		~CSTTEngineImpl();
+
+		void Init();
+		void Shutdown();
+
+		STranscriptResult Transcribe(const float* a_audio_data, int a_audio_length, bool a_get_word_probabilities = false);
+		STranscriptResult TranscribeParts(const float* a_audio_data, const std::vector<int>& a_part_starts, 
+			const std::vector<int>& a_part_ends, bool a_get_word_probabilities = false);
+
+		SLanguageResult DetectLanguage(const float* a_audio_data, int a_audio_length);
+
+		// Setters
+		inline void SetProgressCallback(ProgressCallbackFn a_callback)	{ m_progress_tracker.SetCallback(a_callback);	}
+		inline void Cancel()											{ m_progress_tracker.Cancel();					}
+
+		// Getters
+		inline bool IsLoaded() const									{ return m_context != nullptr;		}
+		inline const SSystemConfig& GetSystemConfig() const				{ return m_config.m_system_config;	}
+		
+
+	private: // Helpers
+		std::string ExtractTextTokens(std::vector<float>& a_probabilities, bool a_get_word_probabilities);
+		void UpdateWhisperContext(whisper_full_params& a_params, int a_part_index);
+		void UpdateWhisperParams(whisper_full_params& a_params, int a_part_index = 0);
+	};
+}
+
